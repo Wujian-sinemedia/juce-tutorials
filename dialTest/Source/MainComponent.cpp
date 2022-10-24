3aceb487dba210c2d17b863798899571df8acac1
Vivek Vijayan <vivekvijayan6282@gmail.com>
2022-10-24 18:26:57 +0530

    update

---
+++ dialTest/Source/MainComponent.cpp
@@ -0,0 +1,47 @@
+#include "MainComponent.h"
+
+//==============================================================================
+MainComponent::MainComponent()
+{
+    dial1.setSliderStyle(juce::Slider::SliderStyle::Rotary);
+    dial1.setTextBoxStyle(juce::Slider::NoTextBox, false, 0, 0);
+    addAndMakeVisible(dial1);
+
+    dial2.setSliderStyle(juce::Slider::SliderStyle::Rotary);
+    dial2.setTextBoxStyle(juce::Slider::NoTextBox, false, 0, 0);
+    addAndMakeVisible(dial2);
+
+    dial3.setSliderStyle(juce::Slider::SliderStyle::Rotary);
+    dial3.setTextBoxStyle(juce::Slider::NoTextBox, false, 0, 0);
+    addAndMakeVisible(dial3);
+
+    dial4.setSliderStyle(juce::Slider::SliderStyle::Rotary);
+    dial4.setTextBoxStyle(juce::Slider::NoTextBox, false, 0, 0);
+    addAndMakeVisible(dial4);
+
+    setSize (600, 100);
+}
+
+MainComponent::~MainComponent()
+{
+    
+}
+
+//==============================================================================
+void MainComponent::paint (juce::Graphics& g)
+{
+    
+}
+
+void MainComponent::resized()
+{
+    juce::Rectangle<int> dialArea = getLocalBounds();
+
+    dial1.setBounds(dialArea.removeFromLeft(getLocalBounds().getWidth() / 4));
+    dial2.setBounds(dialArea.removeFromLeft(getLocalBounds().getWidth() / 4));
+    dial3.setBounds(dialArea.removeFromLeft(getLocalBounds().getWidth() / 4));
+    dial4.setBounds(dialArea.removeFromLeft(getLocalBounds().getWidth() / 4));
+
+    // removeFrom... removes a part from the area and returns the removed area (diak is placed here)
+    // the removed area is reduced from the original area
+}
---
+++ guiTutorial/README.md
@@ -0,0 +1,3 @@
+#Graphics class
+
+<img src="res/build.png" alt="javascript" width="600" height="400"/>
